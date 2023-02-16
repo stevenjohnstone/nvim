cb606153d735d4952b7da6fa1ebfb89563726548
Steven Johnstone <steven.james.johnstone@gmail.com>
2023-02-16 12:05:04 +0000

    Don't autocomplete on CR

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -62,7 +62,7 @@ return {
           ['<c-a>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({
-            select = true,
+            select = false,
           }),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
